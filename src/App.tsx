4a19840cf6e2d80ae533155fb9a10a96e93669be
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2023-10-18 16:53:07 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NextUIProvider } from "@nextui-org/react"
+import { darkTheme } from "./themes"
+
+import { AutocompleteWrapper } from "./components/ui"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<NextUIProvider theme={darkTheme}>
+			<AutocompleteWrapper />
+		</NextUIProvider>
+	)
 }
 
-export default App;
+export default App
