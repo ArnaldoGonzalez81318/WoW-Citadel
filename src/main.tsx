6da5d121f12df3749941f51b622c75139f49d70e
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 14:12:31 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import AppProviders from "@/app/providers/AppProviders"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<AppProviders>
-			<App />
-		</AppProviders>
+		<BrowserRouter
+			future={{
+				v7_startTransition: true,
+				v7_relativeSplatPath: true,
+			}}
+		>
+			<AppProviders>
+				<App />
+			</AppProviders>
+		</BrowserRouter>
 	</React.StrictMode>
 )
