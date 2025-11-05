771a89bd36624f189976b083da90df9a3a63282a
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 14:11:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom"
 import AppShell from "@/components/layout/AppShell"
-import SearchExperience from "@/features/search/components/SearchExperience"
 import { SearchProvider } from "@/features/search/context/SearchContext"
+import AchievementsPage from "@/features/achievements/components/AchievementsPage"
+import ConnectedRealmsPage from "@/features/connectedRealms/components/ConnectedRealmsPage"
+import HomePage from "@/pages/HomePage"
+import NotFoundPage from "@/pages/NotFoundPage"
 
 const App = (): JSX.Element => (
 	<SearchProvider>
 		<AppShell>
-			<SearchExperience />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/achievements" element={<AchievementsPage />} />
+				<Route path="/connected-realms" element={<ConnectedRealmsPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
 		</AppShell>
 	</SearchProvider>
 )
