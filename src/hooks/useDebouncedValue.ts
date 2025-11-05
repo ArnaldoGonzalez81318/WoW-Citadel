af5239bc3f341faf98ba52e369f279178d59031c
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 12:50:01 -0500

    Create useDebouncedValue.ts

---
+++ src/hooks/useDebouncedValue.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+const useDebouncedValue = <T,>(value: T, delay = 400): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const handler = window.setTimeout(() => setDebouncedValue(value), delay)
+    return () => window.clearTimeout(handler)
+  }, [value, delay])
+
+  return debouncedValue
+}
+
+export default useDebouncedValue
