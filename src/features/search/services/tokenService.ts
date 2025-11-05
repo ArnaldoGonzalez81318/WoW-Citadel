980bbee41c6540f38b874a99bfa3380e21a2b3f8
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 12:49:55 -0500

    Create tokenService.ts

---
+++ src/features/search/services/tokenService.ts
@@ -0,0 +1,25 @@
+import { blizzardClient } from "@/lib/blizzardClient"
+import { env } from "@/lib/env"
+
+export type WowTokenPrice = {
+  price: number
+  lastUpdated: Date
+}
+
+type WowTokenResponse = {
+  price: number
+  last_updated_timestamp: number
+}
+
+const namespace = `dynamic-${env.region}`
+
+export const fetchWowTokenPrice = async (): Promise<WowTokenPrice> => {
+  const response = await blizzardClient.get<WowTokenResponse>("/data/wow/token/index", {
+    namespace,
+  })
+
+  return {
+    price: response.price,
+    lastUpdated: new Date(response.last_updated_timestamp),
+  }
+}
