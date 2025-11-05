1f199104e769dbbb057d00cb86c2caad14aca701
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 12:50:20 -0500

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BNET_CLIENT_ID?: string
+  readonly VITE_BNET_CLIENT_SECRET?: string
+  readonly VITE_BNET_ACCESS_TOKEN?: string
+  readonly VITE_BNET_REGION?: string
+  readonly VITE_BNET_LOCALE?: string
+  readonly VITE_BNET_OAUTH_BASE_URL?: string
+  readonly VITE_REACT_APP_CLIENT_ID?: string
+  readonly VITE_REACT_APP_CLIENT_SECRET?: string
+  readonly VITE_REACT_APP_ACCESS_TOKEN?: string
+  readonly VITE_REACT_APP_API_URI?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
