3790339ac476a461a84770b210ac5f70dfe5b14b
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 12:50:07 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import AppProviders from "@/app/providers/AppProviders"
 import App from "./App"
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	// <React.StrictMode>
-	<App />
-	// </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<AppProviders>
+			<App />
+		</AppProviders>
+	</React.StrictMode>
 )
