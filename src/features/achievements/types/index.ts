f8628a135323c13ecf8fafa79b2932c4971d72b1
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-05 14:12:12 -0500

    Create index.ts

---
+++ src/features/achievements/types/index.ts
@@ -0,0 +1,55 @@
+export type LinkReference = {
+  href: string
+}
+
+export type LocalizedString = string | { [locale: string]: string }
+
+export type AchievementSummary = {
+  id: number
+  key: LinkReference
+}
+
+export type AchievementCategorySummary = {
+  id: number
+  name: string
+  key: LinkReference
+}
+
+export type AchievementCategoryIndexResponse = {
+  achievement_categories: AchievementCategorySummary[]
+}
+
+export type AchievementCategory = {
+  id: number
+  name: string
+  achievements?: AchievementSummary[]
+  root_achievements?: AchievementSummary[]
+  subcategories?: AchievementCategorySummary[]
+}
+
+export type Achievement = {
+  id: number
+  name: string
+  description?: string
+  points?: number
+  is_account_wide?: boolean
+  reward?: string
+  reward_item?: {
+    id: number
+    key: LinkReference
+    name?: string
+  }
+  media?: {
+    key: LinkReference
+    id: number
+  }
+}
+
+export type AchievementMediaAsset = {
+  key: string
+  value: string
+}
+
+export type AchievementMedia = {
+  assets: AchievementMediaAsset[]
+}
