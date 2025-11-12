f55792a2f1494240ac8d619ff598e4d36d313253
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2025-11-12 10:09:01 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SearchProvider } from "@/features/search/context/SearchContext"
 import AchievementsPage from "@/features/achievements/components/AchievementsPage"
 import ConnectedRealmsPage from "@/features/connectedRealms/components/ConnectedRealmsPage"
 import HomePage from "@/pages/HomePage"
+import CategoryPage from "@/pages/CategoryPage"
 import NotFoundPage from "@/pages/NotFoundPage"
 
 const App = (): JSX.Element => (
@@ -11,6 +12,7 @@ const App = (): JSX.Element => (
 		<AppShell>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/category-*" element={<CategoryPage />} />
 				<Route path="/achievements" element={<AchievementsPage />} />
 				<Route path="/connected-realms" element={<ConnectedRealmsPage />} />
 				<Route path="*" element={<NotFoundPage />} />
