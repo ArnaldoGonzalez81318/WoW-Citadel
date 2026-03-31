74c93462723c2f6893990dc7b56a590cc56f9c7e
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2026-03-31 01:03:03 -0400

    Create index.ts

---
+++ src/features/mounts/types/index.ts
@@ -0,0 +1,33 @@
+export type LinkReference = {
+  href: string
+}
+
+export type LocalizedString = string | { [locale: string]: string | undefined }
+
+export type MountSummary = {
+  id: number
+  name: string
+  href: string
+  description?: string
+  source?: string
+  displayId?: number
+}
+
+export type MountIndexEntry = {
+  id: number
+  name: string
+  key: LinkReference
+}
+
+export type MountIndexResponse = {
+  mounts: MountIndexEntry[]
+}
+
+export type MountDetail = {
+  id: number
+  name: string
+  description: string
+  source?: string
+  href: string
+  displayId?: number
+}
