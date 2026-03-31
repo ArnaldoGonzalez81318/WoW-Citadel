747f5b12e657320a9011ca3556b551a4c31c64f3
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2026-03-31 01:02:03 -0400

    Update AppProviders.tsx

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { CssBaseline } from "@mui/material"
 import { ThemeProvider } from "@mui/material/styles"
+import { PerformanceOverlayProvider } from "@/devtools/PerformanceOverlayContext"
 import theme from "@/theme"
 
 const queryClient = new QueryClient({
@@ -16,10 +17,12 @@ const queryClient = new QueryClient({
 
 const AppProviders = ({ children }: PropsWithChildren): JSX.Element => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <PerformanceOverlayProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </PerformanceOverlayProvider>
   </QueryClientProvider>
 )
 
