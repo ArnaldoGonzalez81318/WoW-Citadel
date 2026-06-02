ca249dee99eeb91944a8e32bfe246ac7c0d86e0a
Arnaldo Gonzalez <arnaldolgonzalez96@gmail.com>
2026-06-02 15:56:38 -0400

    Format src/main.tsx (style only)
    Apply code formatting to src/main.tsx: normalize indentation, add semicolons, add trailing comma and newline at EOF, and adjust JSX spacing. No functional changes were made.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import AppProviders from "@/app/providers/AppProviders"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppProviders from "@/app/providers/AppProviders";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter
-			future={{
-				v7_startTransition: true,
-				v7_relativeSplatPath: true,
-			}}
-		>
-			<AppProviders>
-				<App />
-			</AppProviders>
-		</BrowserRouter>
-	</React.StrictMode>
-)
+  <React.StrictMode>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
